ee0b3837852328b4ec1bd2d477266865656a4099
TPPiet <timi.pietila@gmail.com>
2024-02-09 16:57:09 +0200

    First commit, deleted stuff from the template, added SQLite, Recipe page for adding and editing recipes, shopping list page started.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using RecipeBook.Data;
 
 namespace RecipeBook
 {
@@ -15,11 +16,20 @@ namespace RecipeBook
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddBlazorBootstrap();
+            // Set path to the SQLite database (it will be created if it does not exist)
+            var dbPath =
+                Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    @"Recipes.db");
+            // Register RecipeService and the SQLite database
+            builder.Services.AddSingleton<RecipeDatabase>(
+                s => ActivatorUtilities.CreateInstance<RecipeDatabase>(s, dbPath));
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
-#endif
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		    builder.Logging.AddDebug();
+            #endif
 
             return builder.Build();
         }
